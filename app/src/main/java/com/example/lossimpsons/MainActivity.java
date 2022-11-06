9e4b826a98051ebc1f3c0f0f88f6e46048ba547f
PabloFP64 <PabloFP64@gmail.com>
2022-11-06 16:45:01 +0100

    botones lleva a cada layout

--- app/src/main/java/com/example/lossimpsons/MainActivity.java
+++ app/src/main/java/com/example/lossimpsons/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lossimpsons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void VentanaPersonajes(View view) {
+        Intent intentPersonajes = new Intent(this, Personajes.class);
+        startActivity(intentPersonajes);
+    }
+
+    public void VentanaRanking(View view) {
+        Intent intentRanking = new Intent(this, Ranking.class);
+        startActivity(intentRanking);
+    }
+
+    public void VentanaJugadores(View view) {
+        Intent intentJugadores = new Intent(this, Jugadores.class);
+        startActivity(intentJugadores);
+    }
+
+    public void VentanaEscenarios(View view) {
+        Intent intentEscenarios = new Intent(this, Escenarios.class);
+        startActivity(intentEscenarios);
+    }
+
+    public void VentanaAcercaDe(View view) {
+        Intent intentAcercaDe = new Intent(this, AcercaDe.class);
+        startActivity(intentAcercaDe);
     }
 }
