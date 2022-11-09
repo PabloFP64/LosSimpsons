8315b00f872c5cd966433c77cd83e7a69a3c9572
PabloFP64 <PabloFP64@gmail.com>
2022-11-09 19:55:27 +0100

    String exportados y añadadido "login falso"

--- app/src/main/java/com/example/lossimpsons/Inicio.java
+++ app/src/main/java/com/example/lossimpsons/Inicio.java
@@ -3,6 +3,7 @@ package com.example.lossimpsons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Inicio extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+    }
+
+    public void Login(View view) {
+        finish();
     }
 }
--- app/src/main/java/com/example/lossimpsons/MainActivity.java
+++ app/src/main/java/com/example/lossimpsons/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, Inicio.class);
+        startActivity(intent);
     }
 
     public void VentanaPersonajes(View view) {
