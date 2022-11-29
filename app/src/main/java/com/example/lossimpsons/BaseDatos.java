40e6d9b1a996cb1c359e1ed49486d37a9b3dd0e1
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-29 20:14:54 +0100

    Update BaseDatos.java

--- app/src/main/java/com/example/lossimpsons/BaseDatos.java
+++ app/src/main/java/com/example/lossimpsons/BaseDatos.java
@@ -1,21 +1,10 @@
 package com.example.lossimpsons;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
-
 
 public class BaseDatos {
 
+    private String connectionString = "mongodb+srv://admin:1234@mydb.24vaewr.mongodb.net/?retryWrites=true&w=majority";
 
-    ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:1234@mydb.24vaewr.mongodb.net/?retryWrites=true&w=majority");
-    MongoClientSettings settings = MongoClientSettings.builder()
-            .applyConnectionString(connectionString)
-            .build();
-    MongoClient mongoClient = (MongoClient) MongoClients.create(settings);
-    MongoDatabase database = mongoClient.getDatabase("MyDB");
 
 
 }
