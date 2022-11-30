d5bf2370ae61ca2d686ac6b9e3db2d5f61af9f01
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-30 18:47:02 +0100

    Metodo para iniciar sesion

--- app/src/main/java/com/example/lossimpsons/Inicio.java
+++ app/src/main/java/com/example/lossimpsons/Inicio.java
@@ -2,18 +2,52 @@ package com.example.lossimpsons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Inicio extends AppCompatActivity {
+
+    EditText usuario, contrasena;
+    Button iniciarSesion, registrarse;
+    BaseDatos db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        usuario = findViewById(R.id.txt_inputUsuario);
+        contrasena = findViewById(R.id.txt_inputContrasena);
+        iniciarSesion = findViewById(R.id.but_login);
+        registrarse = findViewById(R.id.but_register);
+        db = new BaseDatos();
+
+        iniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usr = usuario.getText().toString();
+                String contr = contrasena.getText().toString();
+
+                if (TextUtils.isEmpty(usr) || TextUtils.isEmpty(contr)) {
+                    Toast.makeText(Inicio.this, "Faltan datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean comprobarContrasena = db.comprobarContrasena(usr, contr);
+                    if (comprobarContrasena) {
+                        Toast.makeText(Inicio.this, "Has iniciado sesion correctamente", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Menu.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Inicio.this, "Error de usuario o contraseña", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
-    public void Login(View view) {
-        finish();
-    }
 }
