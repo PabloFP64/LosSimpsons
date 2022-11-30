ba28ca1c305bc3b37979071b94680b7ca0cec321
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-30 18:30:33 +0100

    Clase MongoDB acabada

--- app/src/main/java/com/example/lossimpsons/BaseDatos.java
+++ app/src/main/java/com/example/lossimpsons/BaseDatos.java
@@ -1,9 +1,58 @@
 package com.example.lossimpsons;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 
 public class BaseDatos {
 
     private String connectionString = "mongodb+srv://admin:1234@mydb.24vaewr.mongodb.net/?retryWrites=true&w=majority";
+    private MongoClient mongoClient;
+    private MongoDatabase db;
+
+    public void conectar(){
+        mongoClient = MongoClients.create(connectionString);
+        db = mongoClient.getDatabase("usuarios");
+    }
+
+    public void desconectar(){
+        mongoClient.close();
+    }
+
+    public boolean anadirUsuario(Usuario usuario){
+        Document documento = new Document()
+                .append("nombre", usuario.getNombre())
+                .append("contrasena", usuario.getContrasena())
+                .append("email", usuario.geteMail())
+                .append("usuario", usuario.getUsuario());
+        db.getCollection("usuarios").insertOne(documento);
+        return true;
+    }
+
+    public void eliminarUsuario(String usuario){
+        db.getCollection("usuarios").deleteOne(new Document("nombre", usuario));
+    }
+
+    public boolean comprobarUsuario(String usuario){
+        FindIterable<Document> findIterable = db.getCollection("usuarios").find(new Document("usuario", usuario));
+        if (findIterable.first().isEmpty()){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public boolean comprobarContrasena(String usuario, String contrasena){
+        Document documento = new Document().append("usuario", usuario).append("contrasena", contrasena);
+        if(db.getCollection("usuarios").find(documento).first().isEmpty()){
+            return false;
+        }else return true;
+    }
+
+
 
 
 
